edfd13c6865cf2973d202761e1833a7decf5ba52
Seo MinSeok <minsoku03@inha.edu>
2023-12-11 22:36:21 +0900

    dk wha

--- component---src-pages-404-js-2c4a2b5c409113181b00.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkminsoku_github_io=self.webpackChunkminsoku_github_io||[]).push([[883],{4393:function(e,t,n){n.r(t);var u=n(7294),l=n(421),i=n(6962);t.default=function(){return u.createElement(l.Z,null,u.createElement(i.Z,{title:"404: Not found"}),u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"40404040404"))}}}]);
-//# sourceMappingURL=component---src-pages-404-js-2c4a2b5c409113181b00.js.map
